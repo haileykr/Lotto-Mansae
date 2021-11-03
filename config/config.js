a1d2838db40caa3efef1dbbeb19f1198efc80045
haileykr <haileykmrp@gmail.com>
2021-11-03 22:42:30 +0900

    fix: fix paths

--- config/config.js
+++ config/config.js
@@ -1,27 +1,27 @@
-const dotenv =require("dotenv");
+const dotenv = require("dotenv");
 
 dotenv.config();
 
 module.exports = {
-  "development": {
-    "username": "lotto-manager",
-    "password": process.env.DB_PASSWORD,
-    "database": "lotto_project",
-    "host": "127.0.0.1",
-    "dialect": "mysql"
+  development: {
+    username: "lotto-manager",
+    password: process.env.DB_PASSWORD,
+    database: "lotto_project",
+    host: "127.0.0.1",
+    dialect: "mysql",
   },
-  "test": {
-    "username": "lotto-manager",
-    "password": process.env.DB_PASSWORD,
-    "database": "lotto_project",
-    "host": "127.0.0.1",
-    "dialect": "mysql"
+  test: {
+    username: "lotto-manager",
+    password: process.env.DB_PASSWORD,
+    database: "lotto_project",
+    host: "127.0.0.1",
+    dialect: "mysql",
   },
-  "production": {
-    "username": process.env.HEROKU_DB_USERNAME,
-    "password": process.env.HEROKU_DB_PASSWORD,
-    "database": "lotto_project", 
-    "host": process.env.HEROKU_DB_HOST,
-    "dialect": "mysql"
-  }
-}
+  production: {
+    username: process.env.HEROKU_DB_USERNAME,
+    password: process.env.HEROKU_DB_PASSWORD,
+    database: process.env.HEROKU_DB_DATABASE,
+    host: process.env.HEROKU_DB_HOST,
+    dialect: "mysql",
+  },
+};
